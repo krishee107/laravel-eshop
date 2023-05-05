c82fc7057a3b311fb35acee8f88072e1fc5ab946
krishee107 <krishee107@gmail.com>
2023-05-05 14:26:35 +0200

    Rutas de la aplicación

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('main');
+
+Route::get('products', function () {
+    return 'Esta es la vista del producto';
+})->name('products.index');
+
+Route::get('products/create', function () {
+    return 'Esta es la vista de crear producto';
+})->name('products.create');
+
+Route::post('products', function () {
+    //
+})->name('products.store');
+
+Route::get('products/{product}', function ($product) {
+    return 'Esta es la vista del producto {$product}';
+})->name('products.show');
+
+Route::get('products/{product}/edit', function ($product) {
+    return 'Form para editar el producto {$product}';
+})->name('products.edit');
+
+Route::match(['put', 'patch'], 'products/{product}', function ($product) {
+    //
+})->name('products.update');
+
+Route::delete('products/{product}', function ($product) {
+    //
+})->name('products.destroy');
